986fe43e73e62eeca4a897d717520bc1c441eaad
Lucas <lucasossantossantin@gmail.com>
2023-06-20 17:03:03 -0300

    tranformer

--- internal/market/transformer/transformer.go
+++ internal/market/transformer/transformer.go
@@ -13,5 +13,32 @@ func TransformInput(input dto.TradeInput) *entity.Order {
 		assetPosition := entity.NewInvestorAssetPosition(input.AssetId, input.CurrentShares)
 		investor.AddAssetPosition(assetPosition)
 	}
-	reutrn order
+	return order
+}
+
+func TransformOutput(order *entity.Order) *dto.OrderOutput {
+	output := dto.OrderOutput{
+		OrderId:   order.ID,
+		Investor:  order.Investor.ID,
+		AssetId:   order.Asset.ID,
+		OrderType: order.OrderType,
+		Status:    order.Status,
+		Partial:   order.PendingShares,
+		Shares:    order.Shares,
+	}
+
+	var transactionsOutput []*dto.TransactionOutput
+	for _, t : range order.Transactions {
+		transactionsOutput := &dto.TransactionOutput{
+			TransactionID: t.ID,
+			BuyerID:       t.BuyingOrder.ID,
+			SellerID:      t.SellingOrder.ID,
+			AssetID:       t.SellingOrder.Asset.ID,
+			Price:         t.Price,
+			Shares:        t.SellingOrder.Shares - t.SellingOrder.PendingShares,
+		}
+		transactionsOutput = append(transactionsOutput, transactionOutput)
+	}
+	output.TransactionOutput = transactionsOutput
+	return output
 }
